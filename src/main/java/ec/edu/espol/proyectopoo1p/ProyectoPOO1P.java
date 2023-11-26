d24ff98223a71e63f4bb4d63630545b2cef8c50f
Juan Munizaga <juanmuni@espol.edu.ec>
2023-11-26 12:00:33 -0500

    Inicio de Proyecto

---
+++ src/main/java/ec/edu/espol/proyectopoo1p/ProyectoPOO1P.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.proyectopoo1p;
+
+/**
+ *
+ * @author jmuni
+ */
+public class ProyectoPOO1P {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
