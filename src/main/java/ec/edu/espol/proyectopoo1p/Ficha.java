ab2c9c4c186a52f7d7db5f1e47f498fa3c0680d0
manzannita <152084284+manzannita@users.noreply.github.com>
2023-11-28 21:47:24 -0500

    Update Ficha.java

--- src/main/java/ec/edu/espol/proyectopoo1p/Ficha.java
+++ src/main/java/ec/edu/espol/proyectopoo1p/Ficha.java
@@ -9,5 +9,31 @@ package ec.edu.espol.proyectopoo1p;
  * @author jmuni
  */
 public class Ficha {
+    protected int lado1;
+    protected int lado2;
+
+    public Ficha(int lado1, int lado2) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+
+    public int getLado1() {
+        return lado1;
+    }
+
+    public void setLado1(int lado1) {
+        this.lado1 = lado1;
+    }
+
+    public int getLado2() {
+        return lado2;
+    }
+
+    public void setLado2(int lado2) {
+        this.lado2 = lado2;
+    }
     
+    public String toString(){
+        return lado1+":"+lado2;
+    }
 }
