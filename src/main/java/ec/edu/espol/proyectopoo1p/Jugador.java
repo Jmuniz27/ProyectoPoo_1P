4a025145f6f42c158016a0e60556dd5991af4634
Juan Munizaga <juanmuni@espol.edu.ec>
2023-11-28 21:32:50 -0500

    Creando Clases

---
+++ src/main/java/ec/edu/espol/proyectopoo1p/Jugador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.proyectopoo1p;
+
+/**
+ *
+ * @author jmuni
+ */
+public class Jugador {
+    
+}
